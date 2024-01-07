ca14e87c0aabf64d67b03acd7bffcc0560442150
om957 <78245444+om957@users.noreply.github.com>
2024-01-07 23:08:22 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,8 @@
-$("#messageState").on("change", (x) => {
-	$(".message").removeClass("openNor").removeClass("closeNor");
-	if ($("#messageState").is(":checked")) {
-		$(".message").removeClass("closed").removeClass("no-anim").addClass("openNor");
-		$(".heart").removeClass("closeHer").removeClass("openedHer").addClass("openHer");
-		$(".container").stop().animate({"backgroundColor": "#f48fb1"}, 2000);
-		console.log("Abrindo");
-	} else {
-		$(".message").removeClass("no-anim").addClass("closeNor");
-		$(".heart").removeClass("openHer").removeClass("openedHer").addClass("closeHer");
-		$(".container").stop().animate({"backgroundColor": "#fce4ec"}, 2000);
-		console.log("fechando");
-	}
-});
+const slider = document.querySelector('.slider');
 
-$(".message").on('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
-	console.log("Animation End");
-	if ($(".message").hasClass("closeNor"))
-		$(".message").addClass("closed");
-	$(".message").removeClass("openNor").removeClass("closeNor").addClass("no-anim");
-});
-
-$(".heart").on('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
-	console.log("Animation End");
-	if (!$(".heart").hasClass("closeHer"))
-		$(".heart").addClass("openedHer").addClass("beating");
-	else
-		$(".heart").addClass("no-anim").removeClass("beating");
-	$(".heart").removeClass("openHer").removeClass("closeHer");
-});
+function activate(e) {
+  const items = document.querySelectorAll('.item');
+  e.target.matches('.next') && slider.append(items[0]);
+  e.target.matches('.prev') && slider.append(items[items.ngth -1]);
+}
+document.addEventListener('click',activate,false);
